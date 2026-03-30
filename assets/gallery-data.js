7b5a3690c70ec399216b50b4d838349f71d7dc31
Anurag pal <anupal228@gmail.com>
2026-03-30 20:41:55 +0000

    Add Drive-driven portfolio gallery and site assets

---
+++ assets/gallery-data.js
@@ -0,0 +1,542 @@
+window.GALLERY_ASSETS = [
+  {
+    "id": "13aTLdwWIPgbiFlGxTnOXzUv2Lm4No7vR",
+    "name": "Untitled 279.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1vQaR6nK2uKys1krTVVi9rsSbYiqjlY7Y",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1S8HNq82zUB9mMkMWlkV97zyk2ldLvAib",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1NxVIfj7miK3Uwm_G7FLkF2O4ly9obqgi",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1CgjqRHqguPfL2QEzFxGPthKCwCBOyc4S",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1N0ZbS8toR9asGN3OT3SEYa3Z-xhKxq-E",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "105q5bVsWLsYoUa0sWt7sono5CEaYvfni",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1dFeB_R2Z8cmYm45Zcy2JFsSKaB9lCLSm",
+    "name": "IMG_5700.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1uW-sPLAVpe7iyo8cssYcrJ65x23icaxz",
+    "name": "IMG_5700.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "19OlzD4wKURbN8GvM2-xm-LOsDeK5uSQl",
+    "name": "IMG_5703.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "16qpl9bQhn1ODB0PU6DokNfZ73jbJKOtS",
+    "name": "IMG_5704.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1W_0TgezFBHPQo8rjj9PdR2hqRaawIfEK",
+    "name": "IMG_5713.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "188zoiWRNDA5kIzv_2L5gI2I24VRVSjJH",
+    "name": "IMG_5721.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1OTfRaXrCimarYpQmDxmEXfmRueOpIzbE",
+    "name": "IMG_5736.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1ilPQpvn1lFbxCNmxIT1Q9QVZeysKaqf4",
+    "name": "IMG_5737.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1aLILmuk8ji5NcJRfloZcqW354eAu0x1Z",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1X2JQHhQSlep97SJ2O_XcZe-WrUflbSR6",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1T-OXIxTQRbMKEsElEHkcURkMpiUwNMsp",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1qpRdQ0amoPasRWaMq9UZlRN8poLNYcaD",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "15IyPPbjwTX0L-En8lTafJiYBjtC3ojUw",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1O0ueePgkSJMes3IkGcDbIZEMEW8leLpV",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1CsUJn9ehg9xXDOxZGXEnpegn4spD5YfS",
+    "name": "gym june winggs.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1PRTgbQZygzOM4aZ5ScFig_fdiovOWIKQ",
+    "name": "gym2 winggs june.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "11INvTxe_QVU5RS74CxKgRWchzfewoals",
+    "name": "lv_0_20241120133216.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "19anqBDCIoasw0cAvIqnKjzt8qGlNhypY",
+    "name": "pixelluka apple vs android.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1ibHfMFPffr-bavmxfXFKVnIrMYElSkfm",
+    "name": "2 bcreamy.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1DYoOy568ms3Ewy__gcdcWTFGnodceh9X",
+    "name": "IMG_5685.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1I8N8bFRGFjX0JU1LSjgaB_RuKWH3Dt2A",
+    "name": "IMG_5686.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1Vvg-EljUkT9xp0Dn_DwKc0yF2LsRXeJy",
+    "name": "IMG_5687.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1btL2ehdUlmPn1rbiCzhvMs_NLup2RL9a",
+    "name": "IMG_5688.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1ryExN24iU14DXnaEN1VT62Sx9YNuu_3r",
+    "name": "IMG_5689.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1mYS6xawzf2AZ14rsFfae2xz5kvx9S21f",
+    "name": "IMG_5690.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1U08Hw5DejHllrqv6qKfy9D_oplONinks",
+    "name": "IMG_5691.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1IDsZt71r_Ov2Y6v9ks84tO-Fpb7Jv9jD",
+    "name": "IMG_5692.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1KO1IJdJWS9L9XppSjAW9a0dp7dkKcvm_",
+    "name": "IMG_5693.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1f54miYubfeegQyU4dvaiitZV7ZBzHj8b",
+    "name": "IMG_5694.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1thZq22EcqU00uCVNnBM29GFIF4PC6dfe",
+    "name": "IMG_5695.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1H-4wkVVqumPs2-Q73hoQ_bAJgLRyCzaE",
+    "name": "IMG_5696.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "137MeZyBQWaFLyAASKTc99eFN-V5uQCeJ",
+    "name": "IMG_5697.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1IxuQTlt8t-0nysqA3JOdr8SYVX2x7OiY",
+    "name": "IMG_5698.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1grkI3CjqjBzeVJPKQGxw82EEE_OG46yg",
+    "name": "IMG_5699.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1m7vU482bQWw7EfMcDF0FN7FUjKNItMbJ",
+    "name": "IMG_5700.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1ybR7FsgYuzKquIyAO5h0I4tS4DKMGe1J",
+    "name": "IMG_5701.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1XFTZJ1CN5-XEwgB-kDf9fxNFB8l-PL2u",
+    "name": "IMG_5702.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1wW8xfScklvgsfeDW3ApksRZRCWA1Iyhq",
+    "name": "TGT 6.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1QNyyQnIc-dVGdX-0kP8cM9Ju6tL6Wpxw",
+    "name": "chotu int.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1IeXo6QAe6kHeYb3lrZtyh1gGP2byx35D",
+    "name": "chotu ugc.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1rcxIMGt4MxE2_5ZEp5Awnm9WMf0v3pkC",
+    "name": "my chai kappi.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1Usc-ml0EqeH5PRChx6Ao8fLVmouckeBr",
+    "name": "ugc di.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1IZYc-uUHok02ALNB5OIjvdVe3lTYXtUf",
+    "name": "ugc my.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1BLCFaESUJhRdLJuWxlzds_17B6MTUTBe",
+    "name": "1216 (5).mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1Lb4aFng1-k4iag2kw3bSFE4SBnBopTmP",
+    "name": "combat  tanxshorts.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1cdDB94w_sfPBOCXJlJLluCfRi4IU-JtZ",
+    "name": "winggs libray.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1UQh4P6e4HA-fMp6VYRIWoSPfLlEwQF5r",
+    "name": "winggs pickal 2.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1VP3YZ8GaBxv8roYVBBG-wgLTaaItUNzb",
+    "name": "WINGGS.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1HL5HIQavYRSBV45q25TLFwwy69NnFu8j",
+    "name": "“Crafting Flavors, Sculpting Tools – The Dyo Chef Experience.”.....- chef, bookchef, work, showr.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1ggh8411P5EseUdX5524JkBUpFvcJGyC9",
+    "name": "indore reel gram.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1HDpvCw5ZbJPI4BX_ImWTc4dm3DuZgcTy",
+    "name": "🎁 Naye Retailers ke liye Bumper Welcome Gift!Sirf pehle order par paaiye ₹692 ke SHINSEI toys b.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1hsKuwJYyYp47DBMWECVH1ZxU9Yp2ckgL",
+    "name": "🎉 KHILONAMART BUMPER OFFER – WINNER TIME! 🏆Doston, Bumper Offer khatam, ab bas dhamaka baaki h.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1WuTj9cxDq84RzbOL_uyCoh3wnX6nu4ZZ",
+    "name": "🎯 BOLT se badhaye apna khel ka level!Ek zabardast toy jo sirf fun hi nahi, learning bhi kare su.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1K45dVDAvN2jkHTQ9Nbx21XQhvyb9-2CH",
+    "name": "🧸 Retailers, Kya aap abhi bhi wholesalers ke chakkar mein pareshaan hoAb tension khatam – Khilo.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1vdL1enX1W2_RmEw-p6qzpXw8D_Xez5mm",
+    "name": "🙏 Sabhi ko dil se dhanyavaad!Khilonamart Lucky Draw Event ek super hit aur yaadgaar experience  (1).jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "175XWv6pSrAuc3JAeaRd1O6aXGWzar09s",
+    "name": "🙏 Sabhi ko dil se dhanyavaad!Khilonamart Lucky Draw Event ek super hit aur yaadgaar experience  (2).jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1P8NignN4k8cpy3yYN1LT6fEYpsO_SvXQ",
+    "name": "🙏 Sabhi ko dil se dhanyavaad!Khilonamart Lucky Draw Event ek super hit aur yaadgaar experience .jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1PpEm5YIVfEY6Dj85mrcm6BVsySbuQBUX",
+    "name": "Bachpan Ki Duniya, Khilonamart Se Sajayein! 🎉Battery wale toys ka dhamaka, ab har dukaan pe lag (1).jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1qeIqdJm3X6JJhxsgXWC-r2DHCU_9iDQU",
+    "name": "Bachpan Ki Duniya, Khilonamart Se Sajayein! 🎉Battery wale toys ka dhamaka, ab har dukaan pe lag (2).jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1MNLGj_apgz7ts5GCgn69yT-9erW-Y0f2",
+    "name": "Bachpan Ki Duniya, Khilonamart Se Sajayein! 🎉Battery wale toys ka dhamaka, ab har dukaan pe lag.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1tEr0uf8HsJCvlu5zyADrFk-lT1X_dWCO",
+    "name": "1nand.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1oh6-wHv3GuFAyPLVb_-EXdwKliJ-m_C6",
+    "name": "1126 (1)(1).mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1kyh-9eSMs7kchRMwN21jfpWHxSAIqrwz",
+    "name": "My Video.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1WjbVHtH3Ea0YHAyR0oojLTPWk9DsnKP_",
+    "name": "nandram (2).mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1KmCf2JTmcIpW7uxcDBSTTnZchm5cQp3-",
+    "name": "path 2.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1N86sB__pLzY2xWpVObAVY6qT0EEymssz",
+    "name": "path lab 1.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1bdrRiKMo5RhB6G4b6s3TH9xxUHe2KUKJ",
+    "name": null,
+    "type": "video/mp4"
+  },
+  {
+    "id": "1eByfBe0DpYrzJW55VzeBYlaAz4E4Jxpd",
+    "name": "My Video-2.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1DpLvlJeSXskct_3kd_M8Db2gOPmyonlv",
+    "name": "My Video-2.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1e7NLj9uq0BKzcxz1P8G8QBhuW5IJ3j8u",
+    "name": "turf.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1QDalbniMRtCJ7dItk-lgWrAATxHOUtK6",
+    "name": "Untitled 265.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1uMJ-RNf8ZruzccQla9CbhdRMwL-TcCcn",
+    "name": "Untitled 266.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1JnyrcOLGOQsZGS1iKcHFWE2mmxrnSYaS",
+    "name": "Untitled 267.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1-szttc3za4plXuDsc7hP6cI9lar8mh5i",
+    "name": "Untitled 268.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1-47F0bjka4QFJAMj9vKll-1fOzud7Wax",
+    "name": "Untitled 269.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1mVv2T4z24fGQyC0u8lr63rjvP9XRg_B7",
+    "name": null,
+    "type": "video/mp4"
+  },
+  {
+    "id": "1iM4loBt8SREWQ3hmEuvoV0t37ZpabVU8",
+    "name": null,
+    "type": "video/mp4"
+  },
+  {
+    "id": "1ovuF6O35LqaZ94m3r1C2ITfqAWcpGS14",
+    "name": null,
+    "type": "video/mp4"
+  },
+  {
+    "id": "11FTrPAEhHjKXQGEKTEjPCBQ6slFJHWz_",
+    "name": null,
+    "type": "video/mp4"
+  },
+  {
+    "id": "1_MtQ0OVCZxp2QEs0eW8CbpUrpbKhIqOL",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "16bDNCsZCvTttOTnwdTOn-TuBYXy2_E13",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1VAWq-pspqqv2JEt8FML0oTcxAZDJkH_H",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1ak8ZlPb01o7zfmro3UuHDE6dTFL21wuh",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "10ossLSJn_aNQG3-4FJ2lVRnmXwx_v5Iz",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "10SOmGd-MAm_gvnpCHo-xuUu27gGgkPW4",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1qa_9p3rp9Ck5iCJ4DI_uTr6M83nC_yEo",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1s7zkuzLKAJ9FJxpsLpNtVfk90lpAQoqH",
+    "name": null,
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1b_m0fCIBOTfDdRJjAODU353D44vF9RXk",
+    "name": "_Instagram Reel Cover (1).png",
+    "type": "image/png"
+  },
+  {
+    "id": "1RIGruIidTc05ZX87fKpQHFXR85llUIh3",
+    "name": "_Instagram Reel Cover (2) (1).png",
+    "type": "image/png"
+  },
+  {
+    "id": "1nYODsX4C-Ig57SG96XRm980v4tliokrz",
+    "name": "_Instagram Reel Cover (3) (1).png",
+    "type": "image/png"
+  },
+  {
+    "id": "1Mqazrq6JFziVpAuJKawH8Ejixwe9w5kN",
+    "name": "_Instagram Reel Cover (4) (1).png",
+    "type": "image/png"
+  },
+  {
+    "id": "1D7y_kAAbR1BAjD8fJ78l_aN7pgADfrM-",
+    "name": "_Instagram Reel.png",
+    "type": "image/png"
+  },
+  {
+    "id": "1NPp6LfPYKnert0M5fZ198Pj1V1kl020d",
+    "name": "kite reel thumbnail.jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1eNlkKCT1GVle5_QWKezxaRDaTpWm1I6Q",
+    "name": "rice thumbnail (1).jpg",
+    "type": "image/jpeg"
+  },
+  {
+    "id": "1NZvQoSS8mUpEHaf0rQ1ND62CUJIJSdbC",
+    "name": "bird.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1ganbGKqy4ELqT2yHipu7C9gbFU2TmfFj",
+    "name": "Map.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1cIl4WmyUFLNvoSbav_0KdiXI2vXfpq0c",
+    "name": "Map.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1tPa-Xdntt64M0e_abc-7UvpsKs-_Xfj9",
+    "name": "Navi mumbai.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1BXEUHPAAQhx7StsfVgh2pUxhSjmX8m_e",
+    "name": "rice-1.mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1gHbqSMbzfA4SJGHAX-fIeSBXDDn94eF9",
+    "name": "Untitled 372-1 (1).mp4",
+    "type": "video/mp4"
+  },
+  {
+    "id": "1NWx3fUR4oERrgBBUwqFtYSmDNceY-lEY",
+    "name": "Untitled 376.mp4",
+    "type": "video/mp4"
+  }
+];
---
+++ assets/gallery-render.js
@@ -0,0 +1,80 @@
+function isImage(asset) {
+  return asset.type.startsWith('image/');
+}
+
+function assetUrl(asset) {
+  const base = isImage(asset)
+    ? 'https://drive.google.com/uc?export=view&id='
+    : 'https://drive.google.com/uc?export=download&id=';
+  return `${base}${asset.id}`;
+}
+
+function createImageCard(asset) {
+  const title = asset.name || `Image ${asset.id}`;
+  return `
+    <article class="group overflow-hidden rounded-3xl border border-white/10 bg-surface-container-lowest shadow-2xl">
+      <div class="overflow-hidden bg-black">
+        <img
+          class="w-full h-64 object-cover transition-transform duration-500 group-hover:scale-105"
+          src="${assetUrl(asset)}"
+          alt="${title}"
+          loading="lazy"
+          decoding="async"
+        />
+      </div>
+      <div class="p-5">
+        <h4 class="font-headline text-lg font-bold mb-2">${title}</h4>
+        <p class="text-xs uppercase tracking-[0.3em] text-outline">Image</p>
+      </div>
+    </article>
+  `;
+}
+
+function createVideoCard(asset) {
+  const title = asset.name || `Video ${asset.id}`;
+  return `
+    <article class="group overflow-hidden rounded-3xl border border-white/10 bg-surface-container-lowest shadow-2xl">
+      <div class="overflow-hidden bg-black">
+        <video
+          controls
+          preload="none"
+          class="w-full h-64 object-cover bg-black"
+          src="${assetUrl(asset)}"
+        ></video>
+      </div>
+      <div class="p-5">
+        <h4 class="font-headline text-lg font-bold mb-2">${title}</h4>
+        <p class="text-xs uppercase tracking-[0.3em] text-outline">Video</p>
+      </div>
+    </article>
+  `;
+}
+
+function renderGallery() {
+  if (!window.GALLERY_ASSETS || !Array.isArray(window.GALLERY_ASSETS)) {
+    return;
+  }
+
+  const images = window.GALLERY_ASSETS.filter(isImage);
+  const videos = window.GALLERY_ASSETS.filter((asset) => asset.type === 'video/mp4');
+  const imageCount = document.getElementById('gallery-image-count');
+  const videoCount = document.getElementById('gallery-video-count');
+  const imageGrid = document.getElementById('image-gallery');
+  const videoGrid = document.getElementById('video-gallery');
+
+  if (imageCount) {
+    imageCount.textContent = images.length;
+  }
+  if (videoCount) {
+    videoCount.textContent = videos.length;
+  }
+
+  if (imageGrid) {
+    imageGrid.innerHTML = images.map(createImageCard).join('');
+  }
+  if (videoGrid) {
+    videoGrid.innerHTML = videos.map(createVideoCard).join('');
+  }
+}
+
+document.addEventListener('DOMContentLoaded', renderGallery);
